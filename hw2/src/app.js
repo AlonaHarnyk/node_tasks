82d65d6be817a5bb563b35b6a2af4e7967f777ee
Alona Harnyk <alyonagarnyk@gmail.com>
2024-01-16 12:51:58 +0200

    hw-2_fix-4
    hw-2_fix-4

--- hw2/src/app.js
+++ hw2/src/app.js
@@ -16,9 +16,11 @@ app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use((err, _, res) => {
+app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-export default app;
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
--- hw2/src/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
