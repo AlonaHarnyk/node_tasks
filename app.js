f9d1e3a091943b5212e63dea5ae0462873ee8026
alina513 <130853290+alina513@users.noreply.github.com>
2024-01-27 01:09:56 +0200

    2

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
+  
+  
 module.exports = app;
--- server.js
+++ server.js
@@ -7,7 +7,9 @@ mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
 .then(()=> {console.log("Database connection successful");
-app.listen(PORT)})
+app.listen(PORT, () => {
+    console.log("Server is running. Use our API on port: 3000");
+  })})
 .catch(error => {console.log(error.message);
     process.exit(1);
 })
